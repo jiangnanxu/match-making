074aa28c2eecdebdae4a7e6ee038ea36d1b3dc87
jiangnan xu <s3605002@student.rmit.edu.au>
2018-09-03 13:15:19 +1000

    add columns for age, education, city

--- app/models.py
+++ app/models.py
@@ -13,6 +13,9 @@ class User(db.Model, UserMixin):
      email = db.Column(db.String(120), unique=True,nullable=False)
      image_file = db.Column(db.String(20),nullable=False, default='default.jpg')
      password = db.Column(db.String(60), nullable=False)
+     city = db.Column(db.String(60))
+     age = db.Column(db.Integer)
+     education = db.Column(db.String(60))
      posts = db.relationship('Post', backref='author', lazy=True)
 
      def is_active(self):
