ac3f44ce72c9f4f362028bd91195582ea491ca92
jiangnan xu <s3605002@student.rmit.edu.au>
2018-09-09 16:18:19 +1000

    validate admin login

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,14 @@
 import os
 import secrets
-#from PIL import Image
+
 from flask import render_template, url_for, flash, redirect, request
 from app import app, db, bcrypt
 from app.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from app.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
-posts = [
+
+
+posts1 = [
     {
         'author': 'master yi',
         'title': 'match making 1',
@@ -25,7 +27,8 @@ posts = [
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html', posts=posts)
+    
+    return render_template('home.html')
 
 
 @app.route("/about")
@@ -40,7 +43,7 @@ def register():
     form = RegistrationForm()
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password, city=form.city.data, age=form.age.data,         education=form.education.data)
         db.session.add(user)
         db.session.commit()
         flash('Your account has been created! You are now able to log in', 'success')
@@ -55,10 +58,17 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
-        if user and bcrypt.check_password_hash(user.password, form.password.data):
-            login_user(user, remember=form.remember.data)
-            next_page = request.args.get('next')
-            return redirect(next_page) if next_page else redirect(url_for('home'))
+        
+        if user.email=='admin@gmail.com':
+           if user and bcrypt.check_password_hash(user.password, form.password.data):
+              login_user(user, remember=form.remember.data)
+              next_page = request.args.get('next')
+              return redirect(next_page) if next_page else redirect(url_for('admin'))
+        elif  user and bcrypt.check_password_hash(user.password, form.password.data):
+                login_user(user, remember=form.remember.data)
+                next_page = request.args.get('next')
+                return redirect(next_page) if next_page else redirect(url_for('home'))
+
         else:
             flash('Login Unsuccessful. Please check email and password', 'danger')
     return render_template('login.html', title='Login', form=form)
@@ -70,18 +80,25 @@ def logout():
     return redirect(url_for('home'))
 
 
-def save_picture(form_picture):
-    random_hex = secrets.token_hex(8)
-    _, f_ext = os.path.splitext(form_picture.filename)
-    picture_fn = random_hex + f_ext
-    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
 
-    output_size = (125, 125)
-    i = Image.open(form_picture)
-    i.thumbnail(output_size)
-    i.save(picture_path)
 
-    return picture_fn
+
+
+@app.route("/admin")
+def admin():
+    posts = User.query.all()
+    return render_template('admin.html', title='Admin',posts=posts)
+
+
+@app.route("/admin/<int:id>/delete")
+@login_required
+def deleteuser(id):
+    
+    user=User.query.get_or_404(id)
+    db.session.delete(user)
+    db.session.commit()
+    flash('first user has been deleted!', 'success')
+    return redirect(url_for('home'))
 
 
 @app.route("/account", methods=['GET', 'POST'])
@@ -89,9 +106,6 @@ def save_picture(form_picture):
 def account():
     form = UpdateAccountForm()
     if form.validate_on_submit():
-        if form.picture.data:
-            picture_file = save_picture(form.picture.data)
-            current_user.image_file = picture_file
         current_user.username = form.username.data
         current_user.email = form.email.data
         db.session.commit()
@@ -100,6 +114,7 @@ def account():
     elif request.method == 'GET':
         form.username.data = current_user.username
         form.email.data = current_user.email
-    image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
-    return render_template('account.html', title='Account',
-                           image_file=image_file, form=form)
+    
+    return render_template('account.html', title='Account', form=form)
+    
+      
