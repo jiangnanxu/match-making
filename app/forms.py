11ff3dcd619455d6f31324565897ab7d77d871c1
jiangnan xu <s3605002@student.rmit.edu.au>
2018-09-09 16:19:45 +1000

    include city,age,and education into user database

--- app/forms.py
+++ app/forms.py
@@ -1,46 +1,53 @@
 from flask_wtf import FlaskForm
-from flask_wtf.file import FileField, FileAllowed
 from flask_login import current_user
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, IntegerField
+from wtforms.validators import DataRequired, length, Email, EqualTo, ValidationError
 from app.models import User
 
-
 class RegistrationForm(FlaskForm):
-    username = StringField('Username',
-                           validators=[DataRequired(), Length(min=2, max=20)])
-    email = StringField('Email',
-                        validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password',
+    username = StringField('Username', validators=[DataRequired(), length(min=2, max=20)])
+    
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    
+    password = PasswordField('Password', validators=[DataRequired(),])
+  
+    confirm_password = PasswordField('Confirm Password', 
                                      validators=[DataRequired(), EqualTo('password')])
+
+    city = StringField('City', validators=[DataRequired(), length(min=2, max=20)])
+    
+    age = IntegerField('Age')
+   
+    education = StringField('Education', validators=[DataRequired()])
+
     submit = SubmitField('Sign Up')
-
+    
     def validate_username(self, username):
-        user = User.query.filter_by(username=username.data).first()
-        if user:
-            raise ValidationError('That username is taken. Please choose a different one.')
-
+         user=User.query.filter_by(username=username.data).first()
+         if user:
+            raise ValidationError('that username is taken. please try again')
+    
     def validate_email(self, email):
-        user = User.query.filter_by(email=email.data).first()
-        if user:
-            raise ValidationError('That email is taken. Please choose a different one.')
-
+         user=User.query.filter_by(email=email.data).first()
+         if user:
+            raise ValidationError('that email is taken. please try again')
 
 class LoginForm(FlaskForm):
     email = StringField('Email',
                         validators=[DataRequired(), Email()])
+    
+    
     password = PasswordField('Password', validators=[DataRequired()])
-    remember = BooleanField('Remember Me')
+  
+    remember = BooleanField('remember me')
     submit = SubmitField('Login')
-
 
 class UpdateAccountForm(FlaskForm):
     username = StringField('Username',
-                           validators=[DataRequired(), Length(min=2, max=20)])
+                           validators=[DataRequired(), length(min=2, max=20)])
     email = StringField('Email',
                         validators=[DataRequired(), Email()])
-    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
+    
     submit = SubmitField('Update')
 
     def validate_username(self, username):
@@ -54,3 +61,4 @@ class UpdateAccountForm(FlaskForm):
             user = User.query.filter_by(email=email.data).first()
             if user:
                 raise ValidationError('That email is taken. Please choose a different one.')
+    
