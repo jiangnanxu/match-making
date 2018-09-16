8d5185ff20fb0796f957b319bbb93a137bbb27a4
s3563105 <30428407+s3563105@users.noreply.github.com>
2018-09-16 16:20:35 +1000

    Add files via upload

---
+++ match-making-Feature-Hamad/app/forms.py
@@ -0,0 +1,87 @@
+from flask_wtf import FlaskForm
+from flask_login import current_user
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, IntegerField
+from wtforms.validators import DataRequired, length, Email, EqualTo, ValidationError,number_range
+from app.models import User
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), length(min=2, max=10)])
+    
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    
+    password = PasswordField('Password', validators=[DataRequired(),length(min=3, max=10)])
+  
+    confirm_password = PasswordField('Confirm Password', 
+                                     validators=[DataRequired(), EqualTo('password')])
+
+    city = StringField('City', validators=[DataRequired(), length(min=2, max=10)])
+    
+    age = IntegerField('Age', validators=[DataRequired(), number_range(min=18, max=99)])
+    
+    education = StringField('Education', validators=[DataRequired()])
+
+    submit = SubmitField('Sign Up')
+    
+    def validate_username(self, username):
+         user=User.query.filter_by(username=username.data).first()
+         if user:
+            raise ValidationError('that username is taken. please try again')
+    
+    def validate_email(self, email):
+         user=User.query.filter_by(email=email.data).first()
+         if user:
+            raise ValidationError('that email is taken. please try again')
+      
+class LoginForm(FlaskForm):
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    
+    
+    password = PasswordField('Password', validators=[DataRequired()])
+  
+    remember = BooleanField('remember me')
+    submit = SubmitField('Login')
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username',
+                           validators=[DataRequired(), length(min=2, max=20)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    city = StringField('City', validators=[DataRequired(), length(min=2, max=10)])
+    
+    age = IntegerField('Age', validators=[DataRequired(), number_range(min=18, max=99)])
+    
+    personality = StringField('Personality', validators=[DataRequired()])
+   
+    education = StringField('Education', validators=[DataRequired()])	
+    
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('That username is taken. Please choose a different one.')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('That email is taken. Please choose a different one.')
+
+class PreferencesForm(FlaskForm):
+    prefcity = StringField('City preference', validators=[DataRequired(), length(min=2, max=10)])
+    
+    prefage = IntegerField('Age preference', validators=[DataRequired(), number_range(min=18, max=99)])
+    
+    prefpersonality = StringField('Personality preference', validators=[DataRequired()])
+   
+    prefeducation = StringField('Education preference', validators=[DataRequired()])
+    
+    submit = SubmitField('Update Preferences')
+    
+
+class deleteUserForm(FlaskForm):
+      username = StringField('enter username for delete')
+      submit = SubmitField('Delete')
+    
