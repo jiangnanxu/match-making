a5873cea575daa6211149fe0c9cbefa46f68320d
LiangJiahe233 <43082842+LiangJiahe233@users.noreply.github.com>
2018-09-23 10:53:39 +1000

    Update match.py

--- app/match.py
+++ app/match.py
@@ -15,9 +15,9 @@ def display():
 def main():
 	return render_template('new.html')
 
-@app.route('/input', methods=['GET', 'POST'])
-def importuser():
-	Result.query.delete()
+@app.route('/match', methods=['GET', 'POST'])
+def match():
+	Results.query.delete()
 	fs = User.query.filter_by(current_user.username)
 	out = fs.first()
 	fo = Preferences.query.filter_by(current_user.username)
@@ -28,88 +28,87 @@ def importuser():
 		tper = tout.first()
 		if tper:
 			score = 0
-			if(per.perfage=="All"):
+			if(per.perfage=="all"):
 				score = score + 2
-			if(per.perfage=="EL"):
+			if(per.perfage=="El"):
 				if(users.age==out.age)or(out.age > users.age):
 					score = score + 2
-			if(per.perfage=="BE"):
+			if(per.perfage=="Be"):
 				if(users.age==out.age)or(out.age < users.age):
 					score = score + 2
-			if(per.prefpersonality=="introverted"):
+			if(per.prefpersonality=="in"):
 				if(users.personality=="introverted"):
 					score = score + 2
-				if(users.personality=="neutral"):
+				if(users.personality=="ne"):
 					score = score + 1
-			if(per.prefpersonality=="extroverted"):
+			if(per.prefpersonality=="ex"):
 				if(users.personality=="extroverted"):
 					score = score + 2
-				if(users.personality=="neutral"):
+				if(users.personality=="ne"):
 					score = score + 1
-			if(per.prefpersonality=="neutral"):
+			if(per.prefpersonality=="ne"):
 				if(users.personality=="neutral"):
 					score = score + 2
-				else
+				else:
 					score = score + 1
-			if(per.perfcity=="Same"):
-				if(users.city==out.city):
+			if(per.perfstate=="same"):
+				if(users.state==out.state):
 					score = score + 2
-			if(per.percity=="All"):
+			if(per.percity=="all"):
 				score = score + 2
-			if(per.perfeducation=="High School Graduate"):
+			if(per.perfeducation=="any"):
 				score = score + 2
 			if(per.perfeducation=="Tertiary Degree"):
 				if(users.education==out.education) or (users.income=="Tertiary Degree"):
 					score = score + 2
 			if(per.perfeducation=="Master/Phd"):
-				if(users.education==out.education)
+				if(users.education==out.education):
 					score = score + 2
 			tscore = 0
-			if(tper.perfage=="All"):
+			if(tper.perfage=="all"):
 				tscore = tscore + 2
-			if(tper.perfage=="EL"):
+			if(tper.perfage=="El"):
 				if(users.age==out.age)or(users.age > out.age):
 					tscore = tscore + 2
-			if(tper.perfage=="BE"):
+			if(tper.perfage=="Be"):
 				if(users.age==out.age)or(users.age < out.age):
 					tscore = tscore + 2
-			if(tper.perfcity=="Same"):
-				if(users.city==out.city):
+			if(tper.prefstate=="same"):
+				if(users.state==out.state):
 					tscore = tscore + 2
-			if(tper.perfcity=="All"):
+			if(tper.perfstate=="all"):
 				tscore = tscore + 2
 			if(tper.prefpersonality=="introverted"):
 				if(out.personality=="introverted"):
-					score = score + 2
+					tscore = score + 2
 				if(out.personality=="neutral"):
-					score = score + 1
+					tscore = score + 1
 			if(tper.prefpersonality=="extroverted"):
 				if(out.personality=="extroverted"):
-					score = score + 2
+					tscore = score + 2
 				if(out.personality=="neutral"):
-					score = score + 1
+					tscore = score + 1
 			if(tper.prefpersonality=="neutral"):
 				if(users.personality=="neutral"):
-					score = score + 2
-				else
-					score = score + 1
-			if(tper.perfeduction=="High School Graduate"):
+					tscore = score + 2
+				else:
+					tscore = score + 1
+			if(tper.perfeduction=="any"):
 				tscore = tscore + 2
 			if(tper.perfeducation=="Tertiary Degree"):
 				if(users.education==out.education) or (out.education=="Master/Phd"):
 					tscore = tscore + 2
 			if(tper.perfeducation=="Master/Phd"):
-				if(users.education==out.education)
+				if(users.education==out.education):
 					tscore = tscore + 2
 			sm = float(tscore/6) + float(score/6)
 			fsm = "{:.0%}".format(sm/2)
-			result = Result(user.id, users.name, str(fsm), )
+			result = Result(user.id, users.name, str(fsm))
 			db.session.add(result)
 			db.session.commit()
 		else:
 			return "Object not found"
-	return redirect(url_for('display'))
-	
+	return redirect(url_for('result'))
 	
 			
 
